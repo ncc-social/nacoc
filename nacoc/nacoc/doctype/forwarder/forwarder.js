c5c9e38ebde4798368afba6aa97aa02a58c2bce3
ncc-social <social@ncc.gov.gh>
2022-09-04 12:36:48 +0000

    Custom

--- nacoc/nacoc/doctype/forwarder/forwarder.js
+++ nacoc/nacoc/doctype/forwarder/forwarder.js
@@ -1,8 +1,52 @@
 // Copyright (c) 2022, NACOC and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Forwarder', {
-	// refresh: function(frm) {
+frappe.ui.form.on("Forwarder", "refresh", function(frm) {
+	frappe.ui.form.on("Business Authorisation", {
+			"business": function(frm) {
+			frm.add_fetch("business", "business_name", "business_name");
+			frm.add_fetch("business", "phone_number", "phone_number");
+			frm.refresh_field("business");
 
-	// }
+		}
+	});
+
+});
+
+frappe.ui.form.on(cur_frm.doctype, {
+    refresh: frm => {
+        if (frm.doc.photo_id) {
+            frm.set_df_property("view_photo_id", "options", `
+                <object width="300" height="400" data="${ frm.doc.photo_id }"></object>
+            `);
+        }
+		if (frm.doc.fingerprint_impressions) {
+            frm.set_df_property("view_fingerprint", "options", `
+                <object width="300" height="400" data="${ frm.doc.fingerprint_impressions }"></object>
+            `);
+        }
+    }   
+});
+
+frappe.ui.form.on(cur_frm.doctype, {
+    refresh: function(frm) {
+        if(!frm.is_new()) {
+            cur_frm.add_custom_button(__("Create as Exporter"), function() {
+                frm.events.create_exporter(frm);
+            });
+        }
+    },
+
+    create_exporter: function(frm){
+        frappe.call({
+            method: "scd.security_check_desk.doctype.forwarder.custom.create_exporter",
+            args:{
+                docname: frm.doc.name
+            },
+            callback: function(r){
+                var doc = frappe.model.sync(r.message);
+                frappe.set_route('Form', 'Exporter', r.message.name);
+            }
+        });
+    }
 });
