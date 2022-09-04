f85d99161bd12fe84511ee040566664fd82251e8
ncc-social <social@ncc.gov.gh>
2022-09-04 12:11:53 +0000

    Business

--- nacoc/nacoc/doctype/business_authorisation/business_authorisation.py
+++ nacoc/nacoc/doctype/business_authorisation/business_authorisation.py
@@ -1,8 +1,11 @@
 # Copyright (c) 2022, NACOC and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe, re
 from frappe.model.document import Document
+from frappe.model.naming import make_autoname
 
 class BusinessAuthorisation(Document):
-	pass
+	def autoname(self):
+		suffix = make_autoname("hash", self.doctype)
+		self.name = self.business_name + " (" + suffix + ")"
