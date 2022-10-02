25b1fcf4f46428d9c81e03c582179b9779b2a671
ncc-social <social@ncc.gov.gh>
2022-10-02 09:19:23 +0000

    New doctypes

---
+++ nacoc/rms/doctype/incident/incident.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, NACOC and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Incident', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ nacoc/rms/doctype/judge/judge.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, NACOC and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Judge', {
+	// refresh: function(frm) {
+
+	// }
+});
