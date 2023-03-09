018cd575c1d1aaa6287ffcc9ce666e299e5b17dc
ncc-social <social@ncc.gov.gh>
2023-03-09 21:51:38 +0000

    Pulled in PCU Assessment

---
+++ nacoc/nacoc/doctype/pcu_assessment/pcu_assessment.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, NACOC and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('PCU Assessment', {
+	// refresh: function(frm) {
+
+	// }
+});
