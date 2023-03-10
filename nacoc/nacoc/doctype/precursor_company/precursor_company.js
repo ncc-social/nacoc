ce3e13fdf3c0a02bc2199b9c373a50ba9e40231e
ncc-social <social@ncc.gov.gh>
2023-03-10 21:49:38 +0000

    Change Company to Chemical Company

--- nacoc/nacoc/doctype/precursor_company/precursor_company.js
+++ nacoc/nacoc/doctype/precursor_company/precursor_company.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2023, NACOC and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Company', {
+frappe.ui.form.on('Precursor Company', {
 	// refresh: function(frm) {
 
 	// }
--- nacoc/nacoc/doctype/precursor_company/test_precursor_company.py
+++ nacoc/nacoc/doctype/precursor_company/test_precursor_company.py
@@ -5,5 +5,5 @@
 from frappe.tests.utils import FrappeTestCase
 
 
-class TestCompany(FrappeTestCase):
+class TestPrecursorCompany(FrappeTestCase):
 	pass
