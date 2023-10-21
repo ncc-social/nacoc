e4fa6f4ef6895cdba83b6b273aa5f38ea7b8a56a
ncc-social <social@ncc.gov.gh>
2023-10-21 13:41:37 +0000

    Vessel at Anchorage

---
+++ nacoc/nacoc/doctype/mmu_vessels_at_anchorage/test_mmu_vessels_at_anchorage.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, NACOC and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestMMUVesselsatAnchorage(FrappeTestCase):
+	pass
