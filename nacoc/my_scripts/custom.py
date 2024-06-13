b30888cb9c429e7eb44bd10074c27e30f69f645f
ncc-social <social@ncc.gov.gh>
2024-06-13 15:29:59 +0000

    fix: code changes

--- nacoc/my_scripts/custom.py
+++ nacoc/my_scripts/custom.py
@@ -3,7 +3,7 @@ import frappe
 @frappe.whitelist()
 def get_permitted_regions():
     user_email = frappe.session.user
-    permitted_regions = frappe.get_all("User Permission",filters={"allow": "Region", "user": "tj@ncc.gov.gh"},fields=["name"])
+    permitted_regions = frappe.get_all("User Permission",filters={"allow": "Region", "user": user_email},fields=["name"])
     regions = [frappe.get_value("User Permission", perm["name"], "for_value") for perm in permitted_regions]
     return regions
 
